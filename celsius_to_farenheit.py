559e2f8e82d05ffab0a8b569a0c9a5d95b1c8033
Derakonga <cbarrapalmiericl@gmail.com>
2019-08-15 13:04:53 -0400

    Adding a F° to C° convertor

---
+++ celsius_to_farenheit.py
@@ -0,0 +1,4 @@
+farenheit = int(input("place the farenheit degrees you want to convert to celsius: "))
+
+celsius = (farenheit - 32)/1.8
+print("{}° farenheit are {}° in celsius".format(farenheit, celsius))
